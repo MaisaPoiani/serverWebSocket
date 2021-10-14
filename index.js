f1337ec191a7357d7111d054485a93e49f33e75b
MaisaPoiani <maisampoiani@hotmail.com>
2021-10-14 10:18:09 -0300

    cors e helmet

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+// Config CORS
+var express = require('express')
+  , helmet = require("helmet")    //helmet
+  , cors = require('cors')
+  , app = express();
+ 
+app.options('/products/:id', cors()); // enable pre-flight request for DELETE request 
+app.del('/products/:id', cors(), function(req, res, next){
+  res.json({msg: 'This is CORS-enabled for all origins!'});
+});
+ 
+app.listen(80, function(){
+  console.log('CORS-enabled web server listening on port 80');
+});
+
+app.use(helmet());  //helmet 
+
+// end config CORS
+
 const webSocketsServerPort = 8000;
 const webSocketServer = require('websocket').server;
 const http = require('http');
